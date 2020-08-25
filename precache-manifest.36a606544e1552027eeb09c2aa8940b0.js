56ea72c1f62d5959f8d99284730ad9d0b683313c
navnit0707 <knavnit81@gmail.com>
2020-08-25 22:37:13 +0530

    Updates

---
+++ precache-manifest.36a606544e1552027eeb09c2aa8940b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6ea2132d48ee01c82b49a22168dcbb0",
+    "url": "/childyoucode/index.html"
+  },
+  {
+    "revision": "3d9241ee023df6fd3fa7",
+    "url": "/childyoucode/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "3d9241ee023df6fd3fa7",
+    "url": "/childyoucode/static/js/2.08a5a176.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/childyoucode/static/js/2.08a5a176.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02c473c2712f64442f70",
+    "url": "/childyoucode/static/js/main.c9844fa9.chunk.js"
+  },
+  {
+    "revision": "1b78f7ccc389d211ed5e",
+    "url": "/childyoucode/static/js/runtime-main.9d98332a.js"
+  },
+  {
+    "revision": "5c0ed19f20758b203a48a50ccefb0b42",
+    "url": "/childyoucode/static/media/img2.5c0ed19f.png"
+  }
+]);
